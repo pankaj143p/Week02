4c02b901e96991d28bb6eaa93f94930368f3389c
pankaj143p <pankaj114477pankaj@gmail.com>
2025-01-22 10:42:30 +0530

    3rd problem added of day_04

--- AssistedProblems/BankManagement.java
+++ AssistedProblems/BankManagement.java
@@ -1,26 +1,6 @@
 package AssistedProblems;
-/*
- * Problem 2: Bank and Account Holders (Association)
-Description: Model a relationship where a Bank has Customer objects associated with it. A Customer 
-can have multiple bank accounts, 
-and each account is linked to a Bank.
-Tasks:
-Define a Bank class and a Customer class.
-Use an association relationship to show that each customer has an account in a bank.
-Implement methods that enable communication, such as openAccount() in the Bank class and viewBalance() in the Customer class.
-Goal: Illustrate association by setting up a relationship between customers and the bank.
-
- */
 // Bank class with name
  class Bank{
-    // private String name;
-    // Bank(String name){
-    //     this.name = name;
-    // }
-    // public String getName(){
-    //     return name;
-    // }
-    // openAccount method
     public void openAccount(Customer customer){
         System.out.println("Account opened for " + customer.getName());
     }
@@ -50,8 +30,8 @@ Goal: Illustrate association by setting up a relationship between customers and 
     public void displayAll(){
         System.out.println("Name: " + name + " Account Number: " + accountNumber + " Balance: " + balance);
     }
-
  }
+ 
 public class BankManagement {
     public static void main(String[] args) {
         Bank bank = new Bank();
